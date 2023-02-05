8882e065ea278225cacae178d24322f89bdc33f7
Willyan Bertolino <willyanhpbertolino@gmail.com>
2023-02-05 18:57:17 -0300

    base url api

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,6 @@
 //  Production
 //export const baseURL = 'https://basicapi.willyanhpbertolino.app.br';
-export const baseURL = 'https://seal-app-xsxtl.ondigitalocean.app';
+export const baseURL = 'https://protocols-api-eebmj.ondigitalocean.app/';
 
 // Development
 // export const baseURL = 'http://localhost:5000';
